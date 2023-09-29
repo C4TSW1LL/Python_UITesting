418d33a8ae5c8350d92499f01641ad6ec82df963
CATSWILL <makarchuk008@gmail.com>
2023-09-29 17:48:23 +0400

    Create first tests

--- pages/common_methods.py
+++ pages/common_methods.py
@@ -2,8 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.remote.webelement import WebElement
-from selenium import webdriver
 
 
 class CommonMethods:
